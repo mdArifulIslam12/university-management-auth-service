7e10dd33d086403ea6f6471ade252fd8afa6df5e
mdArifulIslam12 <mdarif767778@gmail.com>
2023-05-30 11:05:56 +0600

    [DONE] Create user interface schema model

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv'
 import path from 'path'
 
-dotenv.config({path: path.join(process.cwd(),'.env')})
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-    port : process.env.PORT,
-    database_url : process.env.DATEBASE_URL
+  port: process.env.PORT,
+  database_url: process.env.DATEBASE_URL,
 }
